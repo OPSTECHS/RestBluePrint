bd553b05d33a0cc3796a4cd8e97c4e7e14e4728a
Isambard Wheeler-artwell <isambardwheeler-artwell@host-199-12.eduroam-local.wifi.le.ac.uk>
2018-06-27 21:38:07 +0100

    Added dependencies for unit testing

---
+++ src/main/java/com/ops/base/RestBluePrint/JUnit/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.ops.base.RestBluePrint.JUnit;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+import com.ops.base.RestBluePrint.Domains.User;
+import com.ops.base.RestBluePrint.Service.UserService;
+
+public class UserServiceTest {
+
+	UserService userService = Mockito.mock(UserService.class);
+
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void TestShouldSaveUser() {
+		
+		UserService us = mock(UserService.class);
+		User u = mock(User.class);
+		
+		us.save(u);
+		
+		
+	}
+	
+}
+
